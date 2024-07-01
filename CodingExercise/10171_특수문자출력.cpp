618668648afdeb3e70dc44cb0cd9d86f9a862c09
Song <84065396+Song-Hyunsub@users.noreply.github.com>
2024-07-01 12:19:04 +0900

    Create 10171_특수문자출력.cpp

---
+++ CodingExercise/10171_특수문자출력.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+/*
+\    /\
+ )  ( ')
+(  /  )
+ \(__)|
+*/
+
+int main() {
+    cout << "\\    /\\\n" ;
+    cout << " )  ( \')\n";
+    cout << "(  /  )\n";
+    cout << " \\(__)|";
+    return 0;
+}
