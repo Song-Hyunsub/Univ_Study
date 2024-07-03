040c5963ac5d537c58432547ded83b3a96b90791
Song <84065396+Song-Hyunsub@users.noreply.github.com>
2024-07-04 02:25:47 +0900

    그리니치 표준시 이용하기

--- CodingExercise/PythonBasic/변수_LAB5.py
+++ CodingExercise/PythonBasic/변수_LAB5.py
@@ -1,15 +1,15 @@
 import time
 
-# 1번을 해보세요!
+# time.time()을 사용하여 변수 fseconds에 저장
 fseconds = time.time()
-# 2번을 해보세요!
+# 전체 초를 계산하여 변수 total_sec에 정수형으로 저장
 total_sec = int(fseconds)
-# 3번을 해보세요!
+# 전체 분을 계산하여 변수 total_min에 저장
 total_min = total_sec // 60
-# 4번을 해보세요!
+# 현재 분을 계산하여 변수 minute에 저장
 minute = total_min % 60
-# 5번을 해보세요!
+# 전체 시를 계산하여 변수 total_hour에 저장
 total_hour = total_min // 60
-# 6번을 해보세요!
+# 현재 시를 계산하여 한국 시간을 계산한 후 변수 hour에 저장하고 출력
 hour = (total_hour + 9) % 24    # 현재 시를 계산한 후 한국 시간을 더함
 print("현재 한국 시간:", str(hour)+"시"+str(minute)+"분", "#시간은 항상 바뀜")
