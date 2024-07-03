5d943a45574a55c3d769a85a05e83ff00efba3cf
Song <84065396+Song-Hyunsub@users.noreply.github.com>
2024-07-04 00:28:51 +0900

    원그리기

---
+++ CodingExercise/PythonBasic/변수_LAB2.py
@@ -0,0 +1,12 @@
+# 1번을 해보세요!
+import turtle
+t = turtle.Turtle()
+t.shape("turtle")
+
+t.speed(1) # 거북이의 속도를 최하로 조절
+# 2번을 해보세요!
+t.color(1, 0, 0)
+# 3번을 해보세요!
+t.begin_fill()
+t.circle(50)
+t.end_fill()
