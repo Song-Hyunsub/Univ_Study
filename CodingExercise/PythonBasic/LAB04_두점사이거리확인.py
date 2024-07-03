0a4561b7dffd40ab8e2add68c34f7e65e0f032c6
Song <84065396+Song-Hyunsub@users.noreply.github.com>
2024-07-04 02:01:56 +0900

    Create LAB04_두점사이거리확인.py

---
+++ CodingExercise/PythonBasic/LAB04_두점사이거리확인.py
@@ -0,0 +1,17 @@
+import turtle
+t = turtle.Turtle()
+t.shape("turtle")
+t.speed(1)
+#1번을 해보세요!
+t.goto(0,0)
+t.setheading(45)
+t.forward(141.4)
+
+#2번을 해보세요!
+t.up()
+t.goto(0, 0)
+t.down()
+t.setheading(0)
+t.forward(100)
+t.setheading(90)
+t.forward(100)
