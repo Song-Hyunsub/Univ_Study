99ae8922058694603b9e619428e72ed09e02108f
Song <84065396+Song-Hyunsub@users.noreply.github.com>
2024-07-04 16:29:53 +0900

    Update 1152_단어개수.cpp

--- CodingExercise/String/1152_단어개수.cpp
+++ CodingExercise/String/1152_단어개수.cpp
@@ -1,3 +1,5 @@
+
+// [Solution 1] EOF 입력 이용하기
 #include <iostream>
 #include <string>
 using namespace std;
@@ -14,3 +16,24 @@ int main(){
     cout << num;
     return 0;
 }
+
+
+/* [Solution 2] getline 입력 이용하기
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	string s;
+	int res = 1;
+	getline(cin, s);
+	if (s.length() == 1 && s[0] == ' ') {
+		cout << 0;
+		return 0;
+	}
+	for (int i = 1; i < s.length() - 1; i++) {
+		if (s[i] == ' ') res++;
+	}
+	cout << res;
+	return 0;
+}
+*/
