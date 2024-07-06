355738c8537d8c22005cde9d1265d230aae89c5c
Song <84065396+Song-Hyunsub@users.noreply.github.com>
2024-07-06 23:53:46 +0900

    Create 2563_2차원배열_색종이.cpp
    꼭 다시 풀어보기!!

---
+++ CodingExercise/Array/2563_2차원배열_색종이.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int a[100][100];
+int main(){
+    int N, x, y; int count = 0;
+    cin >> N;
+    for(int k=0; k<N; k++){
+        cin >> x >> y;
+        for(int i=y; i<y+10; i++){
+            for(int j=x; j<x+10; j++){
+                if(!a[i][j]){
+                    count++;
+                    a[i][j]=1;
+                }
+            }
+        }
+    }
+    cout << count;
+}
