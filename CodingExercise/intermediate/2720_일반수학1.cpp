dd857411d3a04a1cb7bb707fd7d0b6ce51116f3a
Song <84065396+Song-Hyunsub@users.noreply.github.com>
2024-07-09 00:19:41 +0900

    Create 2720_일반수학1.cpp

---
+++ CodingExercise/intermediate/2720_일반수학1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num, T;
+    int arr[4] = {0};
+    cin >> T;
+    while(T>=1){
+        cin >> num;
+        arr[0] = num / 25;
+        arr[1] = (num % 25) / 10;
+        arr[2] = (num % 25) % 10 / 5;
+        arr[3] = ((num % 25) % 10) % 5;
+        for(int i=0; i<4; i++){
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+        T--;
+    }
+    
