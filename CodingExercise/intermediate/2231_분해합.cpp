cc6c148b79b6fd4b8fc19d58554d972957418ee8
Song <84065396+Song-Hyunsub@users.noreply.github.com>
2024-07-12 18:06:40 +0900

    Create 2231_분해합.cpp

---
+++ CodingExercise/intermediate/2231_분해합.cpp
@@ -0,0 +1,26 @@
+/*
+sum(분해합) += num%10;
+num /=10;
+*/
+
+#include <iostream>
+using namespace std;
+int main(){
+    int N;
+    int result = 0;
+    cin >> N;
+    for(int i=0; i<N; i++){
+        int sum = 0;
+        int num = i;
+        while(num!=0){
+            sum += num % 10;
+            num /=10;  
+        }
+        if(sum + i == N){
+            result = i;
+            break;
+        }
+    }
+    cout << result << endl;
+    return 0;
+}
