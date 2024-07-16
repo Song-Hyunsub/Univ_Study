4fd1690974e632eedc8ac502865713bde6555837
Song <84065396+Song-Hyunsub@users.noreply.github.com>
2024-07-16 16:57:50 +0900

    이차함수 그래프 그리기

---
+++ CodingExercise/PythonBasic/함수_LAB6.py
@@ -0,0 +1,18 @@
+#CH08-Lab06 이차함수 그래프 그리기
+
+import turtle 
+t = turtle.Turtle()
+t.shape("turtle")
+
+#1번을 해보세요!
+def f(x):
+    return x**2+1
+
+
+t.goto(200, 0)		#x축 그리기
+t.goto(0, 0)
+t.goto(0, 200)		#y축 그리기
+t.goto(0, 0)
+
+for x in range(150):
+    t.goto(x, int(0.01*f(x)))
