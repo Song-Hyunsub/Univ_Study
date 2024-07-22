935049200102bcef3531a2cacd535d11264537ec
Song <84065396+Song-Hyunsub@users.noreply.github.com>
2024-07-22 13:10:14 +0900

    Add files via upload

---
+++ CodingExercise/PythonBasic/Review/EscapeChar.py
@@ -0,0 +1,19 @@
+print("백문이 불여일견 \n백견이 불여일타")
+
+# 저는 Github입니다.
+print("저는 'Github'입니다.")
+print('저는 "Github"입니다.')
+print("저는 \"Github\"입니다.")
+print("저는 \'Github\'입니다.")
+
+# C:\Users\shs21\OneDrive\py>
+print("C:\\Users\\shs21\\OneDrive\\py>")
+
+# \r : 커서를 맨 앞으로 이동
+print("Red Apple\rPine")
+
+# \b : 백스페이스 (한 글자 삭제)
+print("Redd\bApple")
+
+# \t : 탭
+print("Red\tApple")
