773a35c5b23df77bd6b38124eb7e4e6ee4c05804
Song <84065396+Song-Hyunsub@users.noreply.github.com>
2024-07-23 01:07:34 +0900

    튜플

---
+++ CodingExercise/PythonBasic/Review/Ch5/tuple.py
@@ -0,0 +1,12 @@
+menu = ("돈까스", "치즈까스")
+print(menu[0])
+# 돈까스
+
+print(menu[1])
+# 치즈까스
+
+# menu.add("생선가스") => tuple에서는 add 기능이 불가!!
+
+(name, age, hobby) = ("김종국", 20, "코딩")
+print(name, age, hobby)
+# 김종국 20 코딩
