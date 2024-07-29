997a96b3e1ee3dc8e0b9fba31e69352c5cc22ad0
Song <84065396+Song-Hyunsub@users.noreply.github.com>
2024-07-29 11:22:41 +0900

    Create 실습-1-1_세 정수의 최댓값 구하기.cpp

---
+++ 자료구조/stack/자료구조, 알고리즘 입문/실습-1-1_세 정수의 최댓값 구하기.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(void){
+    int a, b, c;
+    cout << "세 정수의 최댓값을 구합니다.\n" << endl;
+    cout << "a값: ";    cin >> a;
+    cout << "b값: ";    cin >> b;
+    cout << "c값: ";    cin >> c;
+
+    int max = a;
+    if(b > max)  max = b;
+    if(c > max)  max = c;
+
+    cout << "최댓값은 " << max << "입니다." << endl;
+    return 0;
+}
