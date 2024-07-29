f4b3de63e7aee8b5cb9601cc7e5c84304f6ebca9
Song <84065396+Song-Hyunsub@users.noreply.github.com>
2024-07-29 20:06:06 +0900

    Create 연습문제_Q13.cpp

---
+++ 자료구조/stack/자료구조, 알고리즘 입문/ch01_기본알고리즘/연습문제_Q13.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    cout << "----- 곱셈표 -----\n   |";
+    for (int i = 1; i <= 9; i++) cout << setw(3) << i;
+    cout << "\n---+";
+    for (int i = 1; i <= 9; i++) cout << "---";
+    cout << endl;
+
+    for (int i = 1; i <= 9; i++) {
+        cout << setw(3) << i << "|";
+        for (int j = 1; j <= 9; j++) cout << setw(3) << i + j;
+        cout << endl;
+    }
+
+    return 0;
+}
