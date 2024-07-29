65f97958837fab9403d66687ecbd1deda39ba909
Song <84065396+Song-Hyunsub@users.noreply.github.com>
2024-07-29 18:01:55 +0900

    Create 실습-1-6_조건 판단과 분기 살펴보기.cpp

---
+++ 자료구조/stack/자료구조, 알고리즘 입문/ch01_기본알고리즘/실습-1-6_조건 판단과 분기 살펴보기.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int n;
+  cout << "정수: ";   cin >> n;
+  if(n==1)
+    cout << "A" << endl;
+  else if(n==1)
+    cout << "B" << endl;
+  else if(n==2)
+    cout << "C" << endl;
+  else
+    ;
+}
+
+/*
+결과)
+input)
+3
+output)
+
+*/
