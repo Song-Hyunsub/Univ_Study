c6456924b789a228aa322e83b48f808f6c6ed6fd
Song <84065396+Song-Hyunsub@users.noreply.github.com>
2024-07-31 19:21:38 +0900

    그리디 알

---
+++ CodingExercise/intermediate/Silver/11399_ATM.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main(){
+    int N, sum=0;
+    cin >> N;
+    vector<int> time(N);
+    for(int i=0; i<N; i++){
+        cin >> time[i];
+    }
+    sort(time.begin(), time.end());
+    for(int i=0; i<N; i++){
+        sum += (N-i) * time[i];
+    }
+    cout << sum << endl;
+    return 0;
+}
