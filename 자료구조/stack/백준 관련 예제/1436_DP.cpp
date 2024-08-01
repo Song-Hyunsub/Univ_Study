fbd2fd053c0460a9f37fa0b1520693d2e72728f4
Song <84065396+Song-Hyunsub@users.noreply.github.com>
2024-08-01 23:06:01 +0900

    Dynamic Programming

---
+++ 자료구조/stack/백준 관련 예제/1436_DP.cpp
@@ -0,0 +1,40 @@
+/*
+if(X%3==0)
+  X/=3;
+else if(X%2==0)
+  X/=2;
+X--;
+dp 사용
+(1) 초기값 설정: dp[1] = 0
+추가적인 연산이 필요하지 않음
+(2) 점화식 구성:
+dp[i]는 다음 세 가지 방법을 통해 i-1, i/2, i/3 상태에서의 연산 횟수 중 최소값을 이용하여 구할 수 있다.
+만약 i가 3으로 나누어 떨어진다면: dp[i] = dp[i // 3] + 1
+만약 i가 2로 나누어 떨어진다면: dp[i] = dp[i // 2] + 1
+1을 빼는 경우: dp[i] = dp[i - 1] + 1
+위 세 가지 방법 중 최소값을 dp[i]에 저장합니다.
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int make_one(int N){
+    vector<int> dp(N+1, 0);
+    for(int i=2; i<=N; i++){
+        dp[i] = dp[i-1] + 1;
+        if(i%2==0){
+            dp[i] = min(dp[i],  dp[i / 2] + 1);
+        }
+        if(i%3==0){
+            dp[i] = min(dp[i], dp[i / 3] + 1);
+        }
+    }
+    return dp[N];
+}
+int main() {
+    int N;
+    cin >> N;
+    cout << make_one(N) << endl;
+    return 0;
+}
