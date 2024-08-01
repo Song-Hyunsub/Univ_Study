f24191e1fbe8279e0e1b811c5bffd1f5c792119b
Song <84065396+Song-Hyunsub@users.noreply.github.com>
2024-08-01 22:04:00 +0900

    Create 기초트레이닝Day1-3.py

---
+++ CodingExercise/PythonBasic/프로그래머스/기초트레이닝Day1-3.py
@@ -0,0 +1,13 @@
+# Solution 1
+str, n = input().strip().split(' ')
+n = int(n)
+result = '';
+for i in range(0, n):
+    result += str
+print(result)
+
+# Solution 2
+str, n = input().strip().split(' ')
+n = int(n)
+result = str * n
+print(result)
