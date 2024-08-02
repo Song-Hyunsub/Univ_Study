453bda6365254fb01e74a0042dfbc4a697fbdffd
Song <84065396+Song-Hyunsub@users.noreply.github.com>
2024-08-02 17:42:24 +0900

    Update 기초트레이닝Day3-4.py

--- CodingExercise/PythonBasic/프로그래머스/기초트레이닝Day3-4.py
+++ CodingExercise/PythonBasic/프로그래머스/기초트레이닝Day3-4.py
@@ -1,5 +1,7 @@
 def solution(a, b):
-    answer = 0
-    result = str(a) + str(b)
-    answer = int(result)
-    return answer
+    r1 = int(str(a) + str(b))
+    r2 = int(str(b) + str(a))
+    if r1 > r2:
+        return r1
+    else:
+        return r2
