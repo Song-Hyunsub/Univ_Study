53a4d1fd6995aece31eb8b273d7eec3ddbcdacb3
Song <84065396+Song-Hyunsub@users.noreply.github.com>
2024-08-02 19:32:54 +0900

    선형 검색

---
+++ 자료구조/stack/자료구조, 알고리즘 입문/ch03_검색알고리즘/실습3-1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+int search(const int a[], int n, int key)
+{
+  int i=0;
+  while(1){
+    if(i==n) return -1;  // 검색 실패
+    if(a[i]==key) return i;
+    i++;
+  }  
+}
+
+int main()
+{
+  int nx, ky;
+  cout << "선형 검색" << endl;
+  cout << "요소 개수: ";
+  cin >> nx;
+  int* x = new int[nx];  // 요소의 개수가 nx인 int형 배열 x를 생성
+  for(int i=0; i<nx; i++) {
+    cout << "x[" << i << "]: ";
+    cin >> x[i];
+  }
+  cout << "검색값: ";
+  cin >> ky;
+  int idx = search(x, nx, ky);  // 배열 x의 값이 ky인 요소를 선형 검색
+  if(idx == -1) cout << "검색에 실패했습니다." << endl;
+  else cout << ky << "(은)는 x[" << idx << "]에 있습니다." << endl;
+  delete[] x;
+  return 0;
+}
+
+/*
+선형 검색
+요소 개수: 7
+x[0]: 6
+x[1]: 4
+x[2]: 3
+x[3]: 2
+x[4]: 1
+x[5]: 2
+x[6]: 8
+검색값: 2
+2(은)는 x[3]에 있습니다.
+*/
