4e079d62682ddccdfd3512a0a3f04baef399176c
Song <84065396+Song-Hyunsub@users.noreply.github.com>
2024-08-07 16:37:54 +0900

    Create 17626_4squares(DP).cpp

---
+++ CodingExercise/intermediate/Silver/17626_4squares(DP).cpp
@@ -0,0 +1,23 @@
+/*
+dp[0]=0
+j의 범위 => 1~i
+dp[i]=min(dp[i],dp[i−j*j]+1)
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    vector<int> dp(n+1, n);
+    dp[0] = 0;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j*j<=i; j++){
+            dp[i] = min(dp[i], dp[i-j*j]+1);
+        }
+    }
+    cout << dp[n] << endl;
+    return 0;
+}
